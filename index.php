5c32a658315d1cb76deddd8633f0b5504571c292
Calum Raine <calumiscoding@gmail.com>
2022-12-28 00:32:59 +0000

    Added speaker button

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" href="./style.css"/>
 	<script src="./control.js"></script>
 	<script src="./filter.js"></script>
 	<script src="./gain.js"></script>
@@ -8,9 +9,11 @@
 	<script src="./oscillator.js"></script>
 </head>
 <body>
+	<button onclick="synth.wire.connect(synth.ac.destination)">Speakers</button>
 	<button onclick="synth.addOscillator()">Oscillator +</button>
 	<button onclick="synth.addGain()">Gain +</button>
 	<button onclick="synth.addFilter()">Filter +</button>
+	<br/>
 	<script>
 		var synth = new Synth();
 	</script>
