32907995db875d6895dda80b124ab8b520edd1f1
Calum Raine <calumiscoding@gmail.com>
2022-12-31 16:31:46 +0000

    Separated code into files

--- index.php
+++ index.php
@@ -2,22 +2,19 @@
 <html>
 <head>
 	<link rel="stylesheet" href="./style.css"/>
+	<script src="./cable.js"></script>
+	<script src="./module.js"></script>
+	<script src="./envelope.js"></script>
+	<script src="./filter.js"></script>
+	<script src="./gain.js"></script>
+	<script src="./jack.js"></script>
+	<script src="./key.js"></script>
 	<script src="./keyboard.js"></script>
+	<script src="./oscillator.js"></script>
+	<script src="./speakers.js"></script>
 	<script src="./synth.js"></script>
 	<script src="./script.js"></script>
 </head>
 <body>
-	<button onclick="keyboard.addKey(49)">GO!</button>
-	<br/>
-	<br/>
-
-	<button onclick="synth.Output = synth.wire">Speakers</button>
-	<button onclick="synth.addOscillator()">Oscillator +</button>
-	<button onclick="synth.addGain()">Gain +</button>
-	<button onclick="synth.addFilter()">Filter +</button>
-	<br/>
-	<script>
-		//var synth = new Synth();
-	</script>
 </body>
 </html>
