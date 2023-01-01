70316cb87665168ddd29ee947ef885750b968c42
Calum Raine <calumiscoding@gmail.com>
2023-01-01 17:59:05 +0000

    Test with release envelope on gain

--- script.js
+++ script.js
@@ -7,19 +7,39 @@ document.onkeydown = function(event){
 	keyboard.keyDown(event.keyCode);
 };
 
-let osc = synth.addOscillatorKey(true);
-osc.Type = "sawtooth";
+document.onkeyup = function(event){
+	keyboard.keyUp(event.keyCode);
+}
 
-let filter = synth.addFilter(true);
-let gain = synth.addGain(true);
-gain.Gain = 0.2;
+function calumFunction(){
+	let osc = synth.addOscillatorKey(true);
+	osc.Type = "sawtooth";
 
-synth.connect(osc.outputJack, filter.inputJack);
-synth.connect(filter.outputJack, gain.inputJack);
-synth.connect(gain.outputJack, synth.speakers.inputJack);
+	let filter = synth.addFilter(true);
+	let gain = synth.addGain(true);
+	gain.Gain = 0.2;
 
-let env = synth.addEnvelopeRelative(-100);
-env.addWaypoint(0, 0.2);
-env.addWaypoint(50, 0.2);
-env.addWaypoint(0, 0.2);
-synth.connect(env.outputJack, osc.frequencyJack);
+	synth.connect(osc.outputJack, filter.inputJack);
+	synth.connect(filter.outputJack, gain.inputJack);
+	synth.connect(gain.outputJack, synth.speakers.inputJack);
+
+	let fEnv = synth.addEnvelopeRelative(-100);
+	fEnv.addTriggerWaypoint(0, 0.2);
+	fEnv.addTriggerWaypoint(50, 0.2);
+	fEnv.addTriggerWaypoint(0, 0.2);
+	synth.connect(fEnv.outputJack, osc.frequencyJack);
+
+	let gEnv = synth.addEnvelopeAbsolute(0.2);
+	gEnv.addReleaseWaypoint(0, 1);
+	synth.connect(gEnv.outputJack, gain.gainJack);
+
+	let lfo = synth.addOscillatorFixed(false);
+	lfo.Frequency = 2;
+	lfo.Type = "sawtooth";
+
+	let lfoGain = synth.addGain(false);
+	lfoGain.Gain = 1000;
+	synth.connect(lfo.outputJack, lfoGain.inputJack);
+
+	synth.connect(lfoGain.outputJack, filter.frequencyJack);
+}
