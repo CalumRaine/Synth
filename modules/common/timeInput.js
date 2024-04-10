6a29ca67d13663e572c7c115dee67f8ccc23469a
CalumRaine <calumiscoding@gmail.com>
2024-04-10 10:57:26 +0100

    Make static

--- modules/common/timeInput.js
+++ modules/common/timeInput.js
@@ -1,9 +1,10 @@
 class TimeInput extends NumericalInput {
 	static MIN = 0;
-	static MAX = 10000;
+	static MAX = 10000;	// 10 secs
 	static DEFAULT = 0;
+	static UNITS = "ms";
 	constructor(label){
-		super(label, TimeInput.DEFAULT, TimeInput.MIN, TimeInput.MAX, NumericalInput.SLOPE_EXP, "ms");
+		super(label, TimeInput.DEFAULT, TimeInput.MIN, TimeInput.MAX, NumericalInput.SLOPE_EXP, TimeInput.UNITS);
 	}
 }
 
