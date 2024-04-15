c2c22db8eb4089107906ad03918a33dab40f00c1
CalumRaine <calumiscoding@gmail.com>
2024-04-15 12:34:31 +0100

    Add help text

--- modules/arrange/moduleButtons.js
+++ modules/arrange/moduleButtons.js
@@ -7,10 +7,12 @@ class ModuleButtons extends HTMLDivElement {
 
 		this.duplicate = this.appendChild(document.createElement("button"));
 		this.duplicate.setAttribute("type", "button");
+		this.duplicate.setAttribute("title", "Duplicate module");
 		this.duplicate.innerHTML = "+";
 
 		this.remove = this.appendChild(document.createElement("button"));
 		this.remove.setAttribute("type", "button");
+		this.remove.setAttribute("title", "Remove module");
 		this.remove.innerHTML = "-";
 	}
 }
